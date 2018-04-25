f112bd4859ba95b4c2e2870daa15151bde336c65
ooxiaoyan <810978527@qq.com>
2018-04-25 15:41:51 +0800

    feat(controller):add getEmployees Method to EmployeeController

--- src/main/java/com/example/employeeapi/controller/EmployeeController.java
+++ src/main/java/com/example/employeeapi/controller/EmployeeController.java
@@ -1,13 +1,18 @@
 package com.example.employeeapi.controller;
 
 import com.example.employeeapi.datasource.EmployeeRepository;
+import com.example.employeeapi.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @EnableAutoConfiguration
@@ -17,9 +22,8 @@ public class EmployeeController {
 
     @RequestMapping("/employees")
     String getEmployees(Model model) {
-
-        model.addAttribute("message", "hello world");
-
+        List<Employee> allEmployees = employeeRepository.getAllEmployees();
+        model.addAttribute("allEmployees", allEmployees);
         return "employees";
     }
 
